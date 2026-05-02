5ea93d9b0779b755b400fd2a38e7d1a00ae43b40
purvapatil098-max <purvapatil098@gmail.com>
2026-05-02 17:56:23 +0530

    Create 3.2.2.py

---
+++ Practical 3/3.2.2.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# Input matrices
+print("Enter Array1:")
+arr1 = np.array([list(map(int, input().split())) for i in range(3)])
+
+print("Enter Array2:")
+arr2 = np.array([list(map(int, input().split())) for i in range(3)])
+
+
+arr1 = np.array(arr1)
+arr2 = np.array(arr2)
+
+h_stack = np.hstack((arr1, arr2))
+
+v_stack = np.vstack((arr1, arr2))
+print("Horizontal Stack:")
+print(h_stack)
+print("Vertical Stack:")
+print(v_stack)
