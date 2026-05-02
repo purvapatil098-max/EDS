ee81985de5aa4e13f75f49f637ec821e5fa2843c
purvapatil098-max <purvapatil098@gmail.com>
2026-05-02 17:43:04 +0530

    Create 1.1.3.py

---
+++ 01_Python_Basics/1.1.3.py
@@ -0,0 +1,11 @@
+from datetime import date
+
+from datetime import datetime
+
+date1 = input()
+date2 = input()
+
+d1 = datetime.strptime(date1, "%Y-%m-%d")
+d2 = datetime.strptime(date2, "%Y-%m-%d")
+
+print((d2 - d1).days)
