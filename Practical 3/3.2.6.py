dc4cda0acb7cca22e62a946dc4592cde40854507
purvapatil098-max <purvapatil098@gmail.com>
2026-05-02 17:58:18 +0530

    Create 3.2.6.py

---
+++ Practical 3/3.2.6.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# Input array from the user
+array1 = np.array(list(map(int, input().split())))
+
+# Searching
+search_value = int(input("Value to search: "))
+count_value = int(input("Value to count: "))
+broadcast_value = int(input("Value to add: "))
+
+indices = np.where(array1 == search_value)[0]
+print(indices)
+count = np.count_nonzero(array1 == count_value)
+print(count)
+broadcasted = array1 + broadcast_value
+print(broadcasted)
+sorted_array = np.sort(array1)
+print(sorted_array)
