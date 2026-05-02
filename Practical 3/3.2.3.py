827e7b9b9dc9cf1fb927b285eae900c43afb48c6
purvapatil098-max <purvapatil098@gmail.com>
2026-05-02 17:56:53 +0530

    Create 3.2.3.py

---
+++ Practical 3/3.2.3.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Take user input for the start, stop, and step of the sequence
+start = int(input())
+stop = int(input())
+step = int(input())
+
+sequence = np.arange(start, stop, step)
+
+print(sequence)
